5dfc055ab2a1e57000c355fd4f4f6b3849ede4b8
邓剑 <jea@jea-MacBook-Pro.local>
2020-07-31 10:02:34 +0800

    dytrtrt

---
+++ week05/toyBrowser/server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+http.createServer((request, response)=>{
+    let body = [];
+    request.on('error', (err)=>{
+        console.error(err);
+    }).on('data', (chunk)=>{
+        body.push(chunk);
+    }).on('end', ()=>{
+        body = Buffer.concat(body).toString();
+        // console.log("body:", body);
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(
+`<html>
+    <head>
+        <style>
+        .text{color: red;}
+        </style>
+    </head>
+    <body>
+        <div class="text">Hello World!!!!!</div>
+    </body>
+</html>`
+        );
+    })
+}).listen(8087);
+
+console.log("server started434");
