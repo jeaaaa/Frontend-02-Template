fd74d2b06a0da2388cd047faded4409034ea477d
jea <835755354@qq.com>
2020-12-04 11:32:21 +0800

    18

--- week18/publish-server/server.js
+++ week18/publish-server/server.js
@@ -1,17 +1,90 @@
-let http = require("http");
-let fs = require('fs')
+const http = require("http");
+const https = require('https')
+const fs = require('fs')
+const unzipper = require('unzipper')
+const querystring = require('querystring')
 
-http.createServer(function(request, response){
+// auth 路由： 接收code，用code+client_id + client_secret 换token
+function auth(request, response) {
+    // 获取code
+    const query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1])
+    // 使用code 换token
+    getToken(query.code, function (info) {
+        console.log(info)
+        response.write(`<a href="http://localhost:7777/?token=${info.access_token}">publish</a>`)
+        response.end()
+    })
+}
+function getToken(code, callback) {
+    let request = https.request({
+        hostname: 'github.com',
+        path: `/login/oauth/access_token?code=${code}`,
+        port: 443,
+        method: 'POST'
+    }, function (response) {
+        let body = ''
+        response.on('data', chunk => {
+            console.log(chunk.toString())
+            body += chunk.toString()
+        })
+        response.on('end', () => {
+            callback(querystring.parse(body))
+        })
+    })
+    request.end();
+}
+// publish路由： 用token 获取用户信息，检查权限，接受发布
+function publish(request, response) {
+    const query = querystring.parse(request.url.match(/^\/publish\?([\s\S]+)$/)[1])
+    getUser(query.token, info => {
+        if (info.login === '') {
+            request.pipe(unzipper.Extract({ path: '../server/public/sample' }))
+            request.on('end', function () {
+                response.end('yes')
+            })
+
+        } else {
+            alert('please login')
+        }
+    })
+}
+
+function getUser(token, callback) {
+    let request = https.request({
+        hostname: 'api.github.com',
+        path: `/user`,
+        port: 443,
+        method: 'GET',
+        headers: {
+            Authorization: `token ${token}`,
+            "User-Agent": 'test-publish'
+        }
+    }, function (response) {
+        let body = ''
+        response.on('data', chunk => {
+            console.log(chunk.toString())
+            body += chunk.toString()
+        })
+        response.on('end', () => {
+            callback(querystring.parse(body))
+        })
+    })
+    request.end();
+}
+
+http.createServer(function (request, response) {
+    if (request.url.match(/^\/auth\?/)) {
+        return auth(request, response)
+    }
+    if (request.url.match(/^\/publish\?/)) {
+        return publish(request, response)
+    }
+
+
     console.log(request.headers);
 
-    let outFile = fs.createWriteStream("../server/public/index.html");
+    // let outFile = fs.createWriteStream("../server/public/tmp.zip");  //单文件
+    // request.pipe(outFile);   //单文件
 
-    request.on('data', chunk=>{
-        console.log(chunk.toString())
-        outFile.write(chunk)
-    })
-    request.on('end', ()=>{
-        outFile.end();
-        response.end("yes")
-    })
+    // request.pipe(unzipper.Extract({ path: '../server/public/sample' }))
 }).listen(8082)
--- week18/publish-tool/publish.js
+++ week18/publish-tool/publish.js
@@ -1,24 +1,49 @@
-let http = require("http");
-let fs = require('fs');
+const http = require("http");
+const fs = require('fs');
+const archiver = require('archiver');
+const child_process = require('child_process')
+const querystring = require('querystring')
 
-let request = http.request({
-    hostname: "127.0.0.1",
-    port: 8082,
-    method: "POST",
-    headers: {
-        'Content-Type':'application/octet-stream'
-    }
-}, response=>{
-    console.log(response)
-})
+// 打开登录授权页
 
-let file = fs.createReadStream("./sample.html");
+child_process.exec(`open https://github.com/login/oauth/authorize`)
 
-file.on('data', chunk=>{
-    console.log(chunk.toString());
-    request.write(chunk);
-})
-file.on('end', chunk=>{
-    console.log("read finished")
-    request.end(chunk)
-})
+// 创建server 接收鉴权后的token 执行发布程序
+http.createServer(function (request, response) {
+    const query = querystring.parse(request.url.match(/^\/\?([\s\S]+)$/)[1])
+    publish(query.token)
+}).listen(7777)
+
+function publish(token) {    // 鉴权后发布
+    // fs.stat("./sample.html", (err, stats) => {   //单文件处理
+    let request = http.request({
+        hostname: "127.0.0.1",
+        port: 8082,
+        path: `/publish?token=${token}`,
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            // 'Content-Length': stats.size
+        }
+    }, response => {
+        console.log(response)
+    })
+
+    let file = fs.createReadStream("./sample.html");
+
+    const archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+    });
+    archive.directory('./sample/', false);
+    archive.finalize();
+
+    archive.pipe(request);
+
+
+    // file.pipe(request);  //单文件处理
+
+    // file.on('end', () => request.end())  //单文件处理
+    // })
+}
+
+
